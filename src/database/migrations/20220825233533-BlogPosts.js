047ee3acdf1520890ad7c8790891be0984b3372d
Adolligit <adelson.onod@gmail.com>
2022-08-25 21:31:47 -0300

    fix: os testes não rodam localmente (esta uma delicinha como disseram na apresentação de projeto)

--- src/database/migrations/20220825233533-BlogPosts.js
+++ src/database/migrations/20220825233533-BlogPosts.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTalbe('BlogPossts', {
+    await queryInterface.createTable('BlogPossts', {
       id: {
         autoIncrement: true,
         primaryKey: true,
