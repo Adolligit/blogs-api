810c760a9486bcfbd2ca36294f23d87d2359676d
Adolligit <adelson.onod@gmail.com>
2022-08-27 06:53:47 -0300

    feat: (Category: Model) foi criada e testada

---
+++ src/database/models/category.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+  }, {
+    taleName: 'Categories',
+  });
