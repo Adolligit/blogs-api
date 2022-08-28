f439b524c49e30995acc97706783821fa50a034f
Adolligit <adelson.onod@gmail.com>
2022-08-28 16:13:22 -0300

    feat: adicionando decodificação e validação do token a função 'authenticationJWT

--- src/middleware/authentication/CommonUser.js
+++ src/middleware/authentication/CommonUser.js
@@ -1,7 +1,18 @@
-const authenticatioNJWT = (req, res, next) => {
-  console.log('autenticado!');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const authenticationJWT = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(401).json({ message: 'Token not found' });
+
+  try {
+    jwt.verify(authorization, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
 
   next();
 };
 
-module.exports = authenticatioNJWT;
+module.exports = authenticationJWT;
