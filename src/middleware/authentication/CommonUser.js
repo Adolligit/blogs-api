1cadf3a1abf3fe2ad398c570f18c4dba6d60d274
Adolligit <adelson.onod@gmail.com>
2022-08-29 19:06:26 -0300

    fix: transformando 'authenticationJWT' 'arrow function' em função normal

--- src/middleware/authentication/CommonUser.js
+++ src/middleware/authentication/CommonUser.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-const authenticationJWT = (req, res, next) => {
+function authenticationJWT (req, res, next) {
   const { authorization } = req.headers;
 
   if (!authorization) return res.status(401).json({ message: 'Token not found' });
