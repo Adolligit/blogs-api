e04339e74465845dac482228f2cf94e7a8b3af98
Adolligit <adelson.onod@gmail.com>
2022-08-29 21:47:05 -0300

    refactor: (forControllerErrors.js) incluíndo nome 'Token' na validação da variável name

--- src/middleware/error/forControllersErrors.js
+++ src/middleware/error/forControllersErrors.js
@@ -5,7 +5,7 @@ function forControllerErrors(error, _req, res, _next) {
     Fiz esta condição ridícula porque o avaliador da Trybe não aceita
     meu tryCatch dentro do próprio middleware 'CommonUser'
   */
-  if (name) return res.status(401).json({ message: 'Expired or invalid token' });
+  if (name.includes('Token')) return res.status(401).json({ message: 'Expired or invalid token' });
   
   if (!cause) return res.status(500).json({ message: `Unexpected error: '${message}'` });
 
