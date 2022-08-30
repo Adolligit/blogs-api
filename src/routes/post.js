65eab16e443cd93a6fc533e2d4b43344eaf8e17c
Adolligit <adelson.onod@gmail.com>
2022-08-30 15:28:40 -0300

    chore: criando escopo de rotas

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const postController = require('../controllers/post');
 
 const route = express();
+
+route.post('/', postController.create);
+route.post('/:id', postController.byId);
+route.get('/', postController.all);
+route.get('/search?', postController.query);
+route.put('/:id', postController.update);
+route.delete('/:id', postController.remove);
 
 module.exports = route;
