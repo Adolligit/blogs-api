0b6985ff2e74fde45020e34a868a6ed0a7ce7510
Adolligit <adelson.onod@gmail.com>
2023-08-17 16:15:07 -0300

    chore: 8 -> 3 caractere 'displayName'

--- src/util/input-validation.js
+++ src/util/input-validation.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const user = Joi.object({
-    displayName: Joi.string().min(8).required(),
+    displayName: Joi.string().min(3).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
     image: Joi.string(),
