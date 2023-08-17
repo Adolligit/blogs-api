383969a6b91ee66ed412f6e9158131cabd970209
Adolligit <adelson.onod@gmail.com>
2023-08-17 05:52:38 -0300

    feat: (CommonUser) adicionando condição para evitar erro de validação do token Bearer

--- src/middleware/authentication/CommonUser.js
+++ src/middleware/authentication/CommonUser.js
@@ -4,9 +4,17 @@ const jwt = require('jsonwebtoken');
 function authenticationJWT(req, res, next) {
   const { authorization } = req.headers;
 
+  console.log(authorization);
+
   if (!authorization) return res.status(401).json({ message: 'Token not found' });
 
-  const decode = jwt.verify(authorization, process.env.JWT_SECRET);
+  const token = (
+    authorization.includes('Bearer')
+      ? authorization.replace('Bearer', '').trim()
+      : authorization
+  );
+
+  const decode = jwt.verify(token, process.env.JWT_SECRET);
 
   res.locals.authenticated = decode;
 
