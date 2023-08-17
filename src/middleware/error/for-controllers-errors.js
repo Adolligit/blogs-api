d4ddb121ebe2d85e5e5757fce9f802a39b90e375
Adolligit <adelson.onod@gmail.com>
2023-08-17 16:43:12 -0300

    reactor [middleware/errorHandler]: (for-controllers-error) diminuindo código

--- src/middleware/error/for-controllers-errors.js
+++ src/middleware/error/for-controllers-errors.js
@@ -1,21 +1,7 @@
 const httpStatus = require('http-status');
 
-function forControllerErrors(error, _req, res, _next) {
-  const { cause, problem } = error;
-
-  if (problem) {
-    const { status, message } = problem;
-
-    return res.status(status).json({ message });
-  }
-  
-  if (!cause) {
-    return res
-      .status(httpStatus.INTERNAL_SERVER_ERROR)
-      .json({ message: `Unexpected error: '${error.message}'` });
-  }
-
-  return res.status(cause.status).json({ message: error.message });
-}
-
-module.exports = forControllerErrors;
+module.exports = ({ problem }, _req, res, _next) => (
+  problem 
+    ? res.status(problem.status).json({ message: problem.message })
+    : res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Unexpected error' })
+);
