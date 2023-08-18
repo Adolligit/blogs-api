a5d2799bf9448b3644d8a320f73a47027e830696
Adolligit <adelson.onod@gmail.com>
2023-08-18 17:05:49 -0300

    fix: estava fazendo a extração e o tramento do erro de forma incorreta. Corrigido!

--- src/middleware/validation/input-values.js
+++ src/middleware/validation/input-values.js
@@ -1,11 +1,10 @@
 const httpStatus = require('http-status');
 
 const inputValues = (schema) => (req, _res, next) => {
-    const { error: { message } } = schema.validate(req.body);
+    const { error } = schema.validate(req.body);
 
-    if (message) {
-        const error = new Error();
-        error.problem = { message, status: httpStatus.BAD_REQUEST };
+    if (error) {
+        error.problem = { message: error.message, status: httpStatus.BAD_REQUEST };
 
         throw error;
     }
