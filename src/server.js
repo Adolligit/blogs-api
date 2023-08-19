88aafaf086694f29e7618d8dc9adfe520a6257bd
Adolligit <adelson.onod@gmail.com>
2023-08-19 16:15:57 -0300

    feat[dependencies]: cors instalado

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = require('./api');
 
 const server = express();
 const port = process.env.API_PORT || 3000;
 
 server.use(express.json());
+server.use(cors());
 
 server.use('/v1', app);
 
